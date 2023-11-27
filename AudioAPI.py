c0151530f4b62b4719f350c2cec34411ff45fd37
Jordan Bhar <suv300@icloud.com>
2023-11-26 21:37:06 -0500

    removed deloper mode for localhost testing

--- AudioAPI.py
+++ AudioAPI.py
@@ -78,5 +78,5 @@ def upload_audio():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=8000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
+    #app.run(debug=True)
