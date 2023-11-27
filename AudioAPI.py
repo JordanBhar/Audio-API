8609d06b76bf692138cfdbedfdad75b5190eaefa
Jordan Bhar <suv300@icloud.com>
2023-11-26 21:14:33 -0500

    removed uneeded function calls from the API to the ML model

--- AudioAPI.py
+++ AudioAPI.py
@@ -19,26 +19,14 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 class VoiceAnalysisResults:
     def __init__(self):
-        self.syllables = None
-        self.pauses = None
-        self.speech_rate = None
-        self.articulation_rate = None
-        self.speaking_duration = None
-        self.original_duration = None
-        self.balance = None
-        self.f0_mean = None
-        self.f0_std = None
-        self.f0_median = None
-        self.f0_min = None
-        self.f0_max = None
-        self.f0_quantile25 = None
-        self.f0_quantile75 = None
+        self.total_speech_analysis_results = None 
         self.pronunciation_score_percentage = None
         self.gender_analysis_result = None
-        self.total_speech_analysis_results = None 
         # self.prosody_analysis_results = None
-        # self.speech_lev_results = None  # Added for mysplev results
         self.error = None
+        
+        # self.speech_lev_results = None  # Added for mysplev results
+        
 
     def to_dict(self):
         # Convert all attributes to a dictionary
@@ -63,25 +51,12 @@ def upload_audio():
     try:
         voice_analysis = VoiceAnalysisResults()
 
-        # voice_analysis.syllables = mysp.myspsyl(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.pauses = mysp.mysppaus(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.speech_rate = mysp.myspsr(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.articulation_rate = mysp.myspatc(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.speaking_duration = mysp.myspst(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.original_duration = mysp.myspod(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.balance = mysp.myspbala(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.f0_mean = mysp.myspf0mean(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.f0_std = mysp.myspf0sd(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.f0_median = mysp.myspf0med(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.f0_min = mysp.myspf0min(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.f0_max = mysp.myspf0max(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.f0_quantile25 = mysp.myspf0q25(filename_wo_ext, UPLOAD_FOLDER)
-        # voice_analysis.f0_quantile75 = mysp.myspf0q75(filename_wo_ext, UPLOAD_FOLDER)
+        voice_analysis.total_speech_analysis_results = mysp.mysptotal(filename_wo_ext, UPLOAD_FOLDER)
         voice_analysis.pronunciation_score_percentage = mysp.mysppron(filename_wo_ext, UPLOAD_FOLDER)
         voice_analysis.gender_analysis_result = mysp.myspgend(filename_wo_ext, UPLOAD_FOLDER)
-        voice_analysis.total_speech_analysis_results = mysp.mysptotal(filename_wo_ext, UPLOAD_FOLDER)
-
         # voice_analysis.prosody_analysis_results = mysp.myprosody(filename_wo_ext, UPLOAD_FOLDER) 
+
+        # Figure out if this is needed to call
         # voice_analysis.speech_lev_results = mysp.mysplev(filename_wo_ext, UPLOAD_FOLDER)
 
 
